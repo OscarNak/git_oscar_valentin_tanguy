609a5230460963ef94bede41aa5c89d4a3a12034
Valentin PERON <valentin.peron29200@gmail.com>
2022-02-12 14:17:14 +0100

    ajout de la fonction sub qui permet de faire une soustraction

---
+++ calculatrice.py
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+#fonction sub qui permet de faire une soustraction
+def sub(x,y):
+    return x-y
+>>>>>>> 1f7b9cb (ajout de la fonction sub qui permet de faire une soustraction)
+=======
+#fonction sub
+def sub(x,y):
+    return x-y
+>>>>>>> 1ddfd8b (ajout de la fonction sub)
+=======
+#fonction sub
+def sub(x,y):
+    return x-y
+>>>>>>> 1ddfd8b (ajout de la fonction sub)
